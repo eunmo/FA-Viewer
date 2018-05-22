749d5dc8a702a6b1a23aba0a9f0340745945169b
eunmo <eunmo@cs.stanford.edu>
2018-05-23 07:16:50 +0900

    runJavaScriptAlertPanelWithMessage

--- FA Viewer/ViewController.swift
+++ FA Viewer/ViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
 
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        webView.uiDelegate = self
         let url = URL(string: "http://1.235.106.140:3050")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
@@ -23,6 +24,17 @@ class ViewController: UIViewController {
     
     override func prefersHomeIndicatorAutoHidden() -> Bool {
         return true
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let doneAction = UIAlertAction(title: "Close", style: .default, handler: { (_) in
+            completionHandler()
+        })
+        
+        alert.addAction(doneAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
